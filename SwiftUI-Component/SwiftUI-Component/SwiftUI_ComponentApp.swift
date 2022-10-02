6df1f6e931d6393258fe9ea817ffc621b4b2f061
Jack <jack.kuo@crypto.com>
2022-10-03 01:50:28 +0800

    Init project

---
+++ SwiftUI-Component/SwiftUI-Component/SwiftUI_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ComponentApp.swift
+//  SwiftUI-Component
+//
+//  Created by Lien-Tai Kuo on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
